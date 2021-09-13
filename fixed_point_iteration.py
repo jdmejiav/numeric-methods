9c832e400d898d2db9d17072fc8d9b496e198252
jdmejiav <jdmejiav@eafit.edu.co>
2021-09-12 20:59:34 -0500

    Newton and fixed point method

---
+++ fixed_point_iteration.py
@@ -0,0 +1,24 @@
+def f_x(x):
+    return float((x**3)+(2*x**2)+(10*x)-20)          #Put your f_x function here)
+
+
+def g_x(x):
+    return float(-20/(((x**2)+(2*x)+10)))            #Put yout g_x function here
+
+
+if __name__=='__main__':
+    goal = 10^(-6)                      ## Put the goal here
+    max_iteration = 50              ## Put the max iteration you want to achieve (exit parameter)
+    x=50                                 ## Put the initial value of x here
+
+    gx = g_x(x)
+
+    dispersion=x-gx
+    i=0
+    print("Iteration\tx\tg(x)\tdispersion")
+    while (dispersion > 0.5*goal and i < max_iteration):
+        print(str(i)+"\t"+str(x)+"\t"+str(gx)+"\t"+str(dispersion))
+        x = gx
+        gx=g_x(x)
+        dispersion = x-gx
+        i+=1
